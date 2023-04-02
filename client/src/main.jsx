aec0c42aa8871be37460ab9ed40eb60c60b05498
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-02 14:03:10 -0400

    adjust import filename

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // React Router imports
 import { RouterProvider } from "react-router-dom";
-import { router } from "./router.config.js";
+import { router } from "./router.config";
 
 import "./index.css";
 
