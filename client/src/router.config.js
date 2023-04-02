055d8c3f7f1dcb4411edb5d7cefa9ee7d1439ddb
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-01 22:45:12 -0400

    import react-router-dom functionality

--- client/src/router.config.js
+++ client/src/router.config.js
@@ -1,11 +1,2 @@
 // React Router imports
-import { createBrowserRouter } from "react-router-dom";
-// Components
-import Home from "./components/Home";
-
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-]);
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
