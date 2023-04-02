37c4dd346e1fd1190e21346e2c16770e32a2e36d
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-02 14:23:43 -0400

    remove spacing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 // React Router imports
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.config";
-
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
