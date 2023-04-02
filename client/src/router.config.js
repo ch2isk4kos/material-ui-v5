57bbb7789532c15bb598617efeedbf60c0f5a797
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-01 22:31:57 -0400

    create route configuration file

--- client/src/router.config.js
+++ client/src/router.config.js
@@ -2,15 +2,10 @@
 import { createBrowserRouter } from "react-router-dom";
 // Components
 import Home from "./components/Home";
-import Posts from "./components/posts/Posts";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-  },
-  {
-    path: "posts",
-    element: <Posts />,
   },
 ]);
