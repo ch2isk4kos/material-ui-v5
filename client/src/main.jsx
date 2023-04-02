271478e76294dab1c545ca0fff67d08260e93197
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-02 16:58:25 -0400

    import/wrap material container component around root of application

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 // React Router imports
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.config";
+// Material UI Components
+import { Container } from "@mui/material";
 // Custom Components
 import AppBar from "./components/mui/AppBar";
 // import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppBar />
+    <Container maxWidth="lg">
+      <AppBar />
+    </Container>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
