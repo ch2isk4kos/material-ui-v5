08d97dce9bccfe717e3f3e4250d88962025df23c
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-01 23:12:18 -0400

    remove import

--- client/src/router.config.js
+++ client/src/router.config.js
@@ -1,2 +1,11 @@
 // React Router imports
 import { createBrowserRouter } from "react-router-dom";
+// Components
+import App from "./App";
+
+export default router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
