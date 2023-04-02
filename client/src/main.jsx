da2af3e948260582001de181079c2b21da2407f9
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-02 16:21:01 -0400

    change import/component name(s)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 // React Router imports
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.config";
-import "./index.css";
+// Custom Components
+import AppBar from "./components/AppBar";
+// import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <AppBar />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
