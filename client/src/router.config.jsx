da89f08fcfb56c3d8e3f427516839bf59f15ef33
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-02 14:02:07 -0400

    fix: change keyword(s) of export variable definition

---
+++ client/src/router.config.jsx
@@ -0,0 +1,11 @@
+// React Router imports
+import { createBrowserRouter } from "react-router-dom";
+// Components
+import App from "./App";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
