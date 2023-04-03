1bb56a43c8b14c3bf0ee6ba011a2a5ae35dfbc9f
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-03 14:31:15 -0400

    create directory for custom made component

---
+++ client/src/components/custom/layout/main.jsx
@@ -0,0 +1,14 @@
+import AppBar from "../../mui/AppBar";
+import { Outlet } from "react-router-dom";
+import "../../../App.css";
+
+function Layout() {
+  return (
+    <div className="Layout">
+      <AppBar />
+      <Outlet />
+    </div>
+  );
+}
+
+export default Layout;
