edc2757f499ce46d061990e0a479f2819517b931
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-03 14:28:14 -0400

    create component to render on click of answer no from home component

---
+++ client/src/components/Other.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Other = () => {
+  return (
+    <div className="Other">
+      <h1>
+        Then You Can Go
+        <br />
+        Fuck Yourself
+      </h1>
+    </div>
+  );
+};
+
+export default Other;
