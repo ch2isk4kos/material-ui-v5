d648299b5dfac7ecdb323fb86de95425b93079ec
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-03 14:29:33 -0400

    import/render react-router outlet component

--- client/src/components/layout/main.jsx
+++ client/src/components/layout/main.jsx
@@ -1,11 +1,12 @@
 import AppBar from "../mui/AppBar";
+import { Outlet } from "react-router-dom";
 import "../../App.css";
 
-function Layout({ children }) {
+function Layout() {
   return (
     <div className="Layout">
       <AppBar />
-      {children}
+      <Outlet />
     </div>
   );
 }
